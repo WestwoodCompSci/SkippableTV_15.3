66fcb7cad6bff1d14f2a74546ebca8bdce061b40
cjcarroll <cjcarrollswim@gmail.com>
2015-04-24 13:29:14 -0500

    Update classes

--- src/Episode.java
+++ src/Episode.java
@@ -1,4 +1,17 @@
+import java.util.List;
+
 
 public class Episode {
+
+	private String name;
+	private int length;
+	private List<Review> reviews;
+	
+	public Episode(String n, int time, List<Review> r)
+	{
+		name = n;
+		length = time;
+		reviews = r;
+	}
 
 }
---
+++ src/Review.java
@@ -0,0 +1,12 @@
+
+public class Review {
+
+	private int score;
+	private String comment;
+	
+	public Review(int s, String c)
+	{
+		score = s;
+		comment = c;
+	}
+}

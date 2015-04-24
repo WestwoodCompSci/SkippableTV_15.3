7990347e9aafeb5746b1150c075aaf13abc5bb07
reeleese <reeleese3@gmail.com>
2015-04-24 13:55:12 -0500

    Backend - Season

--- src/Season.java
+++ src/Season.java
@@ -1,4 +1,43 @@
+import java.util.List;
+
 
 public class Season {
-
+	//Fields
+	private List<Episode> episodes;
+	private String show;
+	private int seasonNumber;
+	
+	//Constructor
+	public Season(String show, int seasonNumber, List<Episode> episodes)
+	{
+		this.show = show;
+		this.seasonNumber = seasonNumber;
+		this.episodes = episodes;
+	}
+	
+	//Accessors
+	public int getSeasonNumber()
+	{
+		return seasonNumber;
+	}
+	
+	public String getShow()
+	{
+		return show;
+	}
+	
+	public List<Episode> getEpisodes()
+	{
+		return episodes;
+	}
+	
+	public void addEpisode(Episode episode)
+	{
+		
+	}
+	
+	public void addEpisodes(List<Episode> episodes)
+	{
+		
+	}
 }

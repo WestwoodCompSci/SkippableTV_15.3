c5538970cd7dd8b4587befcfac05a1bc327c66fe
Adam Kranz <adam.kranz97@gmail.com>
2015-04-24 13:04:43 -0500

    Played with Encryption, Added Test

---
+++ src/EncryptionTest.java
@@ -0,0 +1,16 @@
+
+public class EncryptionTest {
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		String myPass = "FortyTwo101010";
+		String encPass = Encryption.encrypt(myPass);
+		System.out.println(encPass.length());
+		System.out.println(encPass);
+		String dencPass = Encryption.decrypt(encPass);
+		
+		System.out.println("Password: "+myPass);
+		System.out.println("Encrypted Password: "+encPass);
+		System.out.println("Decrypted Password: "+dencPass);
+	}
+
+}
--- src/encryption.java
+++ src/encryption.java
@@ -4,11 +4,12 @@ import java.security.*;
 import sun.misc.*;
 
 public class Encryption {
-	private static final String ALGO = "AES";
+	private static final String ALGO = "AES/CBC/PKCS5Padding";
 	private static final byte[] keyValue = new byte[]{'F','O','R','T','Y','T','W','O'};
+	private static Key key = generateKey();
 	public static String encrypt(String DATA) throws Exception
 	{
-		Key key = generateKey();
+		
 		Cipher c = Cipher.getInstance(ALGO);
 		c.init(Cipher.ENCRYPT_MODE, key);
 		byte[] encVal = c.doFinal(DATA.getBytes());
@@ -17,9 +18,13 @@ public class Encryption {
 	}
 	public static String decrypt(String DATA) throws Exception
 	{
-		Key key = generateKey();
+	//	System.out.println(DATA.length()%16);
+	//	int pad = 16-(DATA.length()%16);
+	//	for(int i=0;i<pad;i++){DATA+=" ";}
+		
+		System.out.println(DATA.length());
 		Cipher c = Cipher.getInstance(ALGO);
-		c.init(Cipher.DECRYPT_MODE,key);
+		c.init(Cipher.DECRYPT_MODE, key, AlgorithmParameters.getInstance("AES"));
 		byte[] dencVal = c.doFinal(DATA.getBytes());
 		String decryptedValue = new BASE64Encoder().encode(dencVal);
 		return decryptedValue;
@@ -27,7 +32,7 @@ public class Encryption {
 	}
 	public static Key generateKey()
 	{
-		Key key = new SecretKeySpec(keyValue,ALGO);
+		Key key = new SecretKeySpec(keyValue,"AES");
 		return key;
 	}
 }

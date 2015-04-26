3e7f3772173caace1311802d54df210d2676014d
Tristan Seifert <tristan@squee.co>
2015-04-26 14:17:50 -0500

    Wrote some codes for the login dialogue

--- src/Encryption.java
+++ src/Encryption.java
@@ -1,9 +1,38 @@
+import javax.crypto.*;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.*;
+import sun.misc.*;
 
 public class Encryption {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private static final String ALGO = "AES/CBC/PKCS5Padding";
+	private static final byte[] keyValue = new byte[]{'F','O','R','T','Y','T','W','O'};
+	private static Key key = generateKey();
+	public static String encrypt(String DATA) throws Exception
+	{
+		
+		Cipher c = Cipher.getInstance(ALGO);
+		c.init(Cipher.ENCRYPT_MODE, key);
+		byte[] encVal = c.doFinal(DATA.getBytes());
+		String encryptedValue = new BASE64Encoder().encode(encVal);
+		return encryptedValue;
 	}
-
+	public static String decrypt(String DATA) throws Exception
+	{
+	//	System.out.println(DATA.length()%16);
+	//	int pad = 16-(DATA.length()%16);
+	//	for(int i=0;i<pad;i++){DATA+=" ";}
+		
+		System.out.println(DATA.length());
+		Cipher c = Cipher.getInstance(ALGO);
+		c.init(Cipher.DECRYPT_MODE, key, AlgorithmParameters.getInstance("AES"));
+		byte[] dencVal = c.doFinal(DATA.getBytes());
+		String decryptedValue = new BASE64Encoder().encode(dencVal);
+		return decryptedValue;
+		
+	}
+	public static Key generateKey()
+	{
+		Key key = new SecretKeySpec(keyValue,"AES");
+		return key;
+	}
 }
---
+++ src/tv/skippable/gui/LoginDialogue.java
@@ -0,0 +1,110 @@
+package tv.skippable.gui;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+
+public class LoginDialogue extends JDialog {
+	private JTextField username;
+	private JPasswordField password;
+	
+	public LoginDialogue() {
+		this.setTitle("Log In — skippable.tv");
+		this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		
+		// Set minimum size, and centre the window
+		this.setPreferredSize(new Dimension(380, 185));
+		this.setMinimumSize(this.getPreferredSize());
+		
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		screenSize.width /= 2;
+		screenSize.height /= 2;
+
+		screenSize.width -= this.getPreferredSize().width / 2;
+		screenSize.height -= this.getPreferredSize().height / 2;
+		
+		this.setLocation(screenSize.width, screenSize.height);
+		
+		// Create a vertical box into which everything is added
+		Box content = Box.createVerticalBox();
+		
+		// Add a little help text
+		Box introBox = Box.createHorizontalBox();
+
+		introBox.add(Box.createHorizontalStrut(8));
+		introBox.add(new JLabel("Enter your username and password to get started."));
+		introBox.add(Box.createHorizontalStrut(8));
+		
+		content.add(introBox, BorderLayout.NORTH);
+		
+		// Create a panel with spring layout
+		JPanel formPanel = new JPanel(new SpringLayout());
+				
+		// Add the username row
+		JLabel usernameLabel = new JLabel("Username:");
+		formPanel.add(usernameLabel);
+		
+		username = new JTextField();
+		usernameLabel.setLabelFor(username);
+		formPanel.add(username);
+
+		// Add the password row
+		JLabel passwordLabel = new JLabel("Password:");
+		formPanel.add(passwordLabel);
+		
+		password = new JPasswordField();
+		passwordLabel.setLabelFor(password);
+		formPanel.add(password);
+		
+		content.add(formPanel);
+		
+		// create the spring layout grid
+		SpringUtilities.makeCompactGrid(formPanel, 2, 2, 6, 6, 6, 6);
+		
+		// Add the "Forgot Password?", "Log In" and "Cancel" buttons
+		Box buttons = Box.createHorizontalBox();
+		
+		JButton forgot = new JButton("Forgot Password?");
+		buttons.add(forgot);
+		buttons.add(Box.createHorizontalGlue());
+		
+		
+		JButton logIn = new JButton("Log In");
+		buttons.add(logIn);
+		
+		// clicking cancel terminates the application
+		JButton cancel = new JButton("Cancel");
+		cancel.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+		buttons.add(cancel);
+		
+		// Add everything
+		content.add(buttons, BorderLayout.SOUTH);
+		getContentPane().add(content);
+	}	
+	
+	
+	// test method
+	public static void main(String[] args) {
+		// Set the look and feel
+		try {
+			UIManager.setLookAndFeel("com.sun.java.swing.plaf.motif.MotifLookAndFeel");
+		} catch (ClassNotFoundException | InstantiationException
+				| IllegalAccessException | UnsupportedLookAndFeelException e) {
+			e.printStackTrace();
+		}
+		
+		// Create UI
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				LoginDialogue w = new LoginDialogue();
+				w.setVisible(true);
+			}
+		});
+	}
+}

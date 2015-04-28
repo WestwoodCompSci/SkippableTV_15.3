bc634d8f51f225ab763703ea1c255ccf8052c5c5
Riley Taylor <rt.taylor.riley@gmail.com>
2015-04-28 12:55:15 -0500

    Made database class and added some info to fields
    Need backend to make classes if they are in charge of that junk.

---
+++ src/Database.java
@@ -0,0 +1,18 @@
+
+import java.util.*;
+
+
+
+public class Database {
+
+//	ArrayList<user> userList;        waiting on backend to make the userlist	
+//all data stems from list of users
+	
+	
+	
+	public Database()
+	{
+		
+	}
+	
+}

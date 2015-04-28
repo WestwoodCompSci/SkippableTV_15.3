3d6194d2e12e8261fbf7c67dcc2a1a5c00e214bf
Adam Kranz <adam.kranz97@gmail.com>
2015-04-28 14:37:15 -0500

    #FIXED #UPDATED #SWAG

--- src/Encryption.java
+++ src/Encryption.java
@@ -1,33 +1,40 @@
 import javax.crypto.*;
+import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
+
+import com.sun.org.apache.xerces.internal.impl.dv.util.Base64;
+
 import java.security.*;
+import java.util.Arrays;
+
 import sun.misc.*;
 
 public class Encryption {
-	private static final String ALGO = "AES/CBC/PKCS5Padding";
-	private static final byte[] keyValue = new byte[]{'F','O','R','T','Y','T','W','O'};
+	private static final String ALGO = "AES";
+	private static final byte[] keyValue = new byte[]{'F','O','R','T','Y','T','W','O','F','O','R','T','Y','T','W','O'};
 	private static Key key = generateKey();
+	private static String encryptedValue;
 	public static String encrypt(String DATA) throws Exception
 	{
-		
 		Cipher c = Cipher.getInstance(ALGO);
 		c.init(Cipher.ENCRYPT_MODE, key);
 		byte[] encVal = c.doFinal(DATA.getBytes());
-		String encryptedValue = new BASE64Encoder().encode(encVal);
+		encryptedValue = new BASE64Encoder().encode(encVal);
 		return encryptedValue;
 	}
 	public static String decrypt(String DATA) throws Exception
 	{
-	//	System.out.println(DATA.length()%16);
-	//	int pad = 16-(DATA.length()%16);
-	//	for(int i=0;i<pad;i++){DATA+=" ";}
-		
 		System.out.println(DATA.length());
 		Cipher c = Cipher.getInstance(ALGO);
-		c.init(Cipher.DECRYPT_MODE, key, AlgorithmParameters.getInstance("AES"));
-		byte[] dencVal = c.doFinal(DATA.getBytes());
-		String decryptedValue = new BASE64Encoder().encode(dencVal);
-		return decryptedValue;
+		byte[] decryptedValue = new BASE64Decoder().decodeBuffer(encryptedValue);
+		c.init(Cipher.DECRYPT_MODE, key);
+		byte[] dencVal = c.doFinal(decryptedValue);
+		String dencReturn=""; 
+		for(byte x:dencVal)
+		{
+			dencReturn += (char)x;
+		}
+		return dencReturn;
 		
 	}
 	public static Key generateKey()

e9673340a5a97977dc702bac81e8dbf443a7e78d
KManasa <mkthangi@gmail.com>
2015-04-30 13:12:28 -0500

    Add Edits to MainFrame and arrow File

--- src/tv/skippable/gui/MainFrame.java
+++ src/tv/skippable/gui/MainFrame.java
@@ -7,7 +7,10 @@ import java.awt.Font;
 
 import javax.swing.Box;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JProgressBar;
 import javax.swing.JScrollPane;
 import javax.swing.JTextPane;
 import javax.swing.SwingUtilities;
@@ -23,29 +26,28 @@ public class MainFrame extends JFrame
 		this.setPreferredSize(new Dimension(700,500));
 		this.setLocation(450, 200);
 		
+		//vBox
+		Box vBox = Box.createVerticalBox();
 		
 		
-		JScrollPane frame = new JScrollPane(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+		JScrollPane frame = new JScrollPane(vBox,JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
 		
 		this.setContentPane(frame);
 		
 		
-		//vBox
-		Box vBox = Box.createVerticalBox();
-		frame.add(vBox);
 		
 		//More Boxes
-		Box showMenu = Box.createHorizontalBox();
-		Box showTitle = Box.createHorizontalBox();
+		Box menuBox = Box.createHorizontalBox();
+		Box showBox = Box.createHorizontalBox();
 		Box episodes = Box.createHorizontalBox();
 		Box episodeProgress = Box.createHorizontalBox();
 		Box labelTime = Box.createHorizontalBox();
 		Box time = Box.createHorizontalBox();
 		
 		//Adding Boxes
-		vBox.add(showMenu);
+		vBox.add(menuBox);
 		frame.add(Box.createVerticalStrut(10));
-		vBox.add(showTitle);
+		vBox.add(showBox);
 		frame.add(Box.createVerticalStrut(10));
 		vBox.add(episodes);
 		frame.add(Box.createVerticalStrut(10));
@@ -57,7 +59,7 @@ public class MainFrame extends JFrame
 		frame.add(Box.createVerticalStrut(10));
 		
 		
-		//showMenu Box
+		//Menu Box
 		
 		JButton addShow = new JButton ("Add Show...");
 		addShow.setFont(new Font("Times Roman", Font.PLAIN, 40));
@@ -65,11 +67,52 @@ public class MainFrame extends JFrame
 		JButton myShows = new JButton ("My Shows...");
 		myShows.setFont(new Font("Times Roman", Font.BOLD, 40));
 		
-		showMenu.add(Box.createHorizontalStrut(10));
-		showMenu.add(addShow);
-		showMenu.add(Box.createHorizontalStrut(100));
-		showMenu.add(myShows);
-		showMenu.add(Box.createHorizontalStrut(10));
+		menuBox.add(Box.createHorizontalStrut(10));
+		menuBox.add(addShow);
+		menuBox.add(Box.createHorizontalStrut(100));
+		menuBox.add(myShows);
+		menuBox.add(Box.createHorizontalStrut(10));
+		
+		//showBox
+		
+		JLabel title = new JLabel ("Title of Show");
+		title.setFont(new Font("Times Roman", Font.BOLD, 40));
+		
+		String s = "Season 3" + ":" + "" + "2hrs 30 min";
+		
+		JLabel seasonNum = new JLabel (s);
+		JProgressBar seasonPro = new JProgressBar();
+		
+		showBox.add(Box.createHorizontalStrut(10));
+		showBox.add(title);
+		showBox.add(Box.createHorizontalStrut(50));
+		showBox.add(seasonNum);
+		showBox.add(Box.createHorizontalStrut(10));
+		showBox.add(seasonPro);
+		showBox.add(Box.createHorizontalStrut(30));
+		
+		
+		//episodes
+		
+			//png 
+		//episode Progress
+		
+			JLabel question = new JLabel("What Episode?");
+			
+			
+			JComboBox episodeList = new JComboBox ();
+			
+			episodeProgress.add(Box.createHorizontalStrut(10));
+			episodeProgress.add(question);
+			episodeProgress.add(Box.createHorizontalStrut(20));
+			episodeProgress.add(episodeList);
+			episodeProgress.add(Box.createHorizontalStrut(50))
+;			
+		
+		
+		//JLabel showBox = new JLabel (show)
+		
+		
 		
 		
 		

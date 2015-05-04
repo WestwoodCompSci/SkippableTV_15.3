c79c49a04be9d206ff1084e2a040baabe7ca62b3
Riley Taylor <rt.taylor.riley@gmail.com>
2015-05-04 14:25:18 -0500

    Lots of changes
    I honestly dont know

--- src/Database.java
+++ src/Database.java
@@ -5,46 +5,70 @@ import org.json.simple.*;
 
 public class Database {
 
-//	ArrayList<User> userList;        waiting on backend to make the user class with all methods necessary	
+	static ArrayList<User> userList;        //should this be static? eclipse told me to make it static	
 //	data stems from list of users
 	
-//	ArrayList<TVShow> showList;
+	ArrayList<TVShow> showList;
 //	data stems from user inputs into show collection
 	
 	
 	public Database()
 	{
-		
+		userList = new ArrayList<User>();
+		showList = new ArrayList<TVShow>();
 	}
 
-	public void addUser(//User a)
+	public List<TVShow> getshowList()
 	{
-		//userList.add(a);
+		return showList;
 	}
+	
+	public List<User> getuserList()
+	{
+		return userList;
+	}
+	
+	//public void addUser(User a)
+	//{
+	//	userList.add(a);
+	//}
 	
 	public void sortListAlpha()
 	{
 		//implement method that will search through user list to alphabetically sort all users
 	}
 	
-	public void deleteUser(// User a)
-	{
-			//userList.remove(a);
-	}
+	//public void deleteUser(User a)
+	//{
+	//	userList.remove(a);
+	//}
 		
 	
-	public void addReview(//Review r, TVShow s)
+	public void addReview(Review r, Episode e)
 	{
-		
+		e.getReviews().add(r);
 	}
 	
 		
 	//ADD JSON METHODS TO ADD STUFF TO THE DATABASE TEXT DOCUMENT TO WRITE DATA FROM LISTS TO FILE
 	
-	//                   http://www.json.org/            <--json.org = muy bueno
+	//                  http://www.json.org/            <--json.org = muy bueno
 	
+	//					https://code.google.com/p/json-simple/wiki/EncodingExamples			<-- json library api
 	
+	public static void main (String[] args)
+	{
+		Database thisOne = new Database();
+		TVShow House_of_Cards = new TVShow("House of Cards", new ArrayList<Season>());
+		thisOne.getshowList().add(House_of_Cards);
+		User user1 = new User("dankLord69" , "420alldayeveryday");
+		userList.add(user1);
 		
+	}
 		
+	
 		
 }
+
+
+	

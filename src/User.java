743af7320a2b7f923fecb944eeb3d3b7b1abd605
cjcarroll <cjcarrollswim@gmail.com>
2015-05-04 14:04:02 -0500

    user

--- src/User.java
+++ src/User.java
@@ -1,13 +1,22 @@
+import java.util.List;
+
 
 public class User {
 
-	String username;
-	String password;
+	private String username;
+	private String password;
+	private List<TVShow> showList;
 	
 	public User(String user, String pass)
 	{
 		username = user;
 		password = pass;
 	}
+	
+	public void addShow(TVShow show)
+	{
+		showList.add(show);
+	}
+	
 	
 }

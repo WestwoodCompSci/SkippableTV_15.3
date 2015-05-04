5b37c4ec8c03d7b18e60888d08871c5224ff7d7a
Riley Taylor <rt.taylor.riley@gmail.com>
2015-05-04 12:36:02 -0500

    New classes and arrays
    Necessary improvements

--- src/Database.java
+++ src/Database.java
@@ -1,18 +1,47 @@
 
 import java.util.*;
-
+import org.json.simple.*;
 
 
 public class Database {
 
-//	HashMap<user> userMap;        waiting on backend to make the userlist	
-//all data stems from list of users
+//	ArrayList<User> userList;        waiting on backend to make the user class with all methods necessary	
+//	data stems from list of users
 	
+//	ArrayList<TVShow> showList;
+//	data stems from user inputs into show collection
 	
 	
 	public Database()
 	{
 		
 	}
+
+	public void addUser(//User a)
+	{
+		//userList.add(a);
+	}
 	
+	public void sortListAlpha()
+	{
+		//implement method that will search through user list to alphabetically sort all users
+	}
+	
+	public void deleteUser(// User a)
+	{
+			//userList.remove(a);
+	}
+		
+	
+	public void addReview(//Review r, TVShow s)
+	{
+		
+	}
+	
+		
+		
+		
+		
+		
+		
 }

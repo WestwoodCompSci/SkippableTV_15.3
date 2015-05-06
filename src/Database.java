60e479c146eb159a71b0bbc4212e28179806c61d
laoqimi <laoqimi@gmail.com>
2015-05-06 12:45:28 -0500

    added sort method
    : /\ ]

--- src/Database.java
+++ src/Database.java
@@ -42,13 +42,22 @@ public class Database {
 	
 	public void sortListAlpha()
 	{
-		//implement method that will search through user list to alphabetically sort all users
+		for (int c = 0; c < userList.size(); c++) {
+		      for (int d = 0; d < userList.size() - c; d++) {
+		        if (userList.get(d).getUsername().compareTo(userList.get(d+1).getUsername()) < 0) /* For descending order use < */
+		        {
+		          User swap  = userList.get(d);
+		          userList.set(d, userList.get(d+1));
+		          userList.set(d+1,swap);
+		        }
+		      }
+		    }
 	}
 	
-	//public void deleteUser(User a)
-	//{
-	//	userList.remove(a);
-	//}
+	public void deleteUser(User a)
+	{
+		userList.remove(a);
+	}
 		
 	
 	public void addReview(Review r, Episode e)

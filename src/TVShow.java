7228327d4498f1b572cd13b560efa8b6689eda64
reeleese <reeleese3@gmail.com>
2015-05-13 01:08:45 -0500

    Added the stuff to sort episodes by episodeNumber
    Somebody who knows more than me about quicksort should probably verify
    this...

--- src/TVShow.java
+++ src/TVShow.java
@@ -7,17 +7,15 @@ public class TVShow {
 	private String name;
 	private List <Episode> episodes;
 	private int length; //in minutes
+	
+	
+	
 	public TVShow(String n, List <Episode> eps)
 	{
 		name = n;
 		episodes = eps;
 		epSort();
 		updateLength();
-	}
-	
-	private void epSort()
-	{
-		
 	}
 	
 	public String getName()
@@ -30,11 +28,31 @@ public class TVShow {
 		return episodes;
 	}
 	
+	public int getLength()
+	{
+		return length;
+	}
+	
 	public void addEpisode(Episode ep)
 	{
 		episodes.add(ep);
 		updateLength();
 		epSort();
+	}
+	
+	public void epSort()
+	{
+		QuickSort sorter = new QuickSort();
+		Episode[] toSort = new Episode[episodes.size()];
+		for(int i = 0; i < toSort.length; i++) {
+			toSort[i] = episodes.get(i);
+		}
+		Episode[] sorted = sorter.quickSort(toSort);
+		List<Episode> finalList = new ArrayList<Episode>();
+		for (Episode x : sorted) {
+			finalList.add(x);
+		}
+		episodes = finalList;
 	}
 	
 	public void addEpisodes(List<Episode> eps)
@@ -56,11 +74,6 @@ public class TVShow {
 		}
 		
 		length = total;
-	}
-	
-	public int getLength()
-	{
-		return length;
 	}
 	
 	public List<Episode> bestEpsRating(double minRating)
@@ -86,5 +99,53 @@ public class TVShow {
 			time += show
 		}
 		return null;
+	}
+	
+	private class QuickSort {
+		private Episode[] episodes;
+		private int length;
+		
+		private Episode[] quickSort(Episode[] stuff)
+		{
+			if(stuff == null || stuff.length == 0)
+				return;
+			episodes = stuff;
+			length = stuff.length;
+			sortHelper(0, length - 1);
+			return episodes;
+		}
+		
+		private void sortHelper(int lo, int hi)
+		{
+			int i = lo;
+			int j = hi;
+			
+			Episode pivot = episodes[lo + (hi - lo) / 2];
+			
+			while(i <= j) {
+				
+				while(episodes[i].compareTo(pivot) < 0) {
+					i++;
+				}
+				while(episodes[j].compareTo(pivot) > 0) {
+					j--;
+				}
+				
+				if (i <= j)  {
+					Episode help = episodes[i];
+					episodes[i] = episodes[j];
+					episodes[j] = help;
+					i++;
+					j--;
+				}
+			}
+			
+			if (lo < j) {
+				sortHelper(lo, j);
+			}
+			if (i < hi) {
+				sortHelper(i, hi);
+			}
+		}
 	}
 }

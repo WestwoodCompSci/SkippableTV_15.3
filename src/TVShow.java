7c49acf8e9eef4331ceda7ffb8228df9e577c462
reeleese <reeleese3@gmail.com>
2015-05-14 12:44:51 -0500

    added rateSort
    sorts episodes by rating

--- src/TVShow.java
+++ src/TVShow.java
@@ -55,6 +55,21 @@ public class TVShow {
 		}
 	}
 	
+	public void rateSort()
+	{
+		QuickSort sorter = new QuickSort();
+		Episode[] toSort = new Episode[episodes.size()];
+		for(int i = 0; i < toSort.length; i++) {
+			toSort[i] = episodes.get(i);
+		}
+		
+		Episode[] sorted = sorter.quickSort2(toSort);
+		episodes.clear();
+		for (Episode x : sorted) {
+			episodes.add(x);
+		}
+	}
+	
 	public void addEpisodes(List<Episode> eps)
 	{
 		for(Episode x : eps)
@@ -105,6 +120,7 @@ public class TVShow {
 		private Episode[] episodes;
 		private int length;
 		
+		//used for epSort
 		private Episode[] quickSort(Episode[] stuff)
 		{
 			if(stuff == null || stuff.length == 0)
@@ -123,11 +139,10 @@ public class TVShow {
 			Episode pivot = episodes[lo + (hi - lo) / 2];
 			
 			while(i <= j) {
-				
-				while(episodes[i].compareTo(pivot) < 0) {
+				while(episodes[i].epCompare(pivot) < 0) {
 					i++;
 				}
-				while(episodes[j].compareTo(pivot) > 0) {
+				while(episodes[j].epCompare(pivot) > 0) {
 					j--;
 				}
 				
@@ -145,6 +160,50 @@ public class TVShow {
 			}
 			if (i < hi) {
 				sortHelper(i, hi);
+			}
+		}
+		
+		//used for rateSort
+		private Episode[] quickSort2 (Episode[] stuff)
+		{
+			if(stuff == null || stuff.length == 0)
+				return null;
+			episodes = stuff;
+			length = stuff.length;
+			sortHelper2(0, length - 1);
+			return episodes;
+		}
+		
+		private void sortHelper2 (int lo, int hi)
+		{
+
+			int i = lo;
+			int j = hi;
+			
+			Episode pivot = episodes[lo + (hi - lo) / 2];
+			
+			while(i <= j) {
+				while(episodes[i].rateCompare(pivot) < 0) {
+					i++;
+				}
+				while(episodes[j].rateCompare(pivot) > 0) {
+					j--;
+				}
+				
+				if (i <= j)  {
+					Episode help = episodes[i];
+					episodes[i] = episodes[j];
+					episodes[j] = help;
+					i++;
+					j--;
+				}
+			}
+			
+			if (lo < j) {
+				sortHelper2(lo, j);
+			}
+			if (i < hi) {
+				sortHelper2(i, hi);
 			}
 		}
 	}

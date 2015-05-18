81003bc97072982aff81906c5863cbd8f2701019
KManasa <mkthangi@gmail.com>
2015-05-18 14:24:00 -0500

    More stuff

--- src/tv/skippable/gui/MyShowsWindow.java
+++ src/tv/skippable/gui/MyShowsWindow.java
@@ -3,11 +3,17 @@ package tv.skippable.gui;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.*;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 import javax.swing.plaf.metal.*;
+
+import tv.skippable.backend.Episode;
+import tv.skippable.backend.Review;
+import tv.skippable.backend.TVShow;
 
 public class MyShowsWindow extends JFrame 
 {
@@ -24,12 +30,24 @@ public class MyShowsWindow extends JFrame
 				
 			this.setContentPane(frame);
 			
+			//testing for list actionListener
+			ArrayList<Episode> episodeList= new ArrayList<Episode>();
+			Episode e = new Episode("Ep1", 1, 1, 23, null);
+			episodeList.add(e);
+			TVShow show1 = new TVShow("show1", episodeList); 
+			String name1 = show1.getName();
+			TVShow[] myShows = {show1};
+			String[] myShowsName = {name1};
+			//-------------------------------
+			
 			String[] potato = {
 					"test 1", "yolo the obamacare",
 					"nein pls"
+					
+					//can we change this to an array of TVShows instead of String? I think it makes it easier to implement later...and then the JList just displays the TVShow title?
 			};
 				
-			JList list = new JList(potato); //data has type Object[]
+			JList list = new JList(myShowsName); //data has type Object[]
 			list.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
 			list.setLayoutOrientation(JList.VERTICAL);
 			list.setVisibleRowCount(-1);
@@ -44,12 +62,21 @@ public class MyShowsWindow extends JFrame
 
 				@Override
 				public void valueChanged(ListSelectionEvent arg0) {
-					// TODO Auto-generated method stub
-					
-					//store what we selected to be implemented on Main Frame
 					
 					
-					//dispose();
+					//store what we selected to be implemented on Main Frame:
+					String value = (String) list.getSelectedValue();
+					int i = list.getSelectedIndex();
+					TVShow selectedShow = myShows[i];
+					System.out.println(selectedShow.getName());
+					
+					
+					MainFrame frame = new MainFrame();
+					frame.addShowGUI(selectedShow);
+					
+					dispose();
+					
+					//i have no idea if any of this is right/feasible...plz make changes as necessary...or get rid of it all.
 				}
 
 				

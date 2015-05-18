c8b5575ce83d401d039b79cfa6cf2a2eec7bdb72
LegitMikeyG <michael.goolsbey@gmail.com>
2015-05-18 12:51:53 -0500

    Testing
    Testing

--- src/Testing.java
+++ src/Testing.java
@@ -1,4 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
 
-public class Testing {
+import tv.skippable.backend.Review;
+import tv.skippable.backend.TVShow;
+import tv.skippable.backend.Episode;
 
+
+public class Testing 
+{
+
+	//Shouldn't each TV show have an array of seasons?
+	
+	public static void main(String[] args)
+	{
+		
+		
+		List<Episode> episodes = new ArrayList<Episode>();
+		TVShow test = new TVShow("The Office", episodes);
+		List<Review> reviews = new ArrayList<Review>();
+		reviews.add(new Review(5, "10/10"));
+		test.addEpisode(new Episode("title", 1, 1, 23, new ArrayList<Review>()));
+		List<Episode> eps = test.getEpisodes();
+		Episode episode = eps.get(0);
+		
+		System.out.println(episode.getReviews().get(0).getName());
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+			
+			
 }

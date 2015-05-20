868bde3fc577ba5c67a3374dfdd0c21043e4cb5f
PattyGuo <joowonyong@gmail.com>
2015-05-20 12:34:01 -0500

    Updated Network

--- src/tv/skippable/network/ClientNetwork.java
+++ src/tv/skippable/network/ClientNetwork.java
@@ -1,40 +1,87 @@
 package tv.skippable.network;
 //import User;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.PrintWriter;
 import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+import tv.skippable.backend.TVShow;
+import tv.skippable.backend.User;
 
 
 public class ClientNetwork {
 	
 	String hostname = "55F9622";
-	ObjectInputStream in;
-	ObjectOutputStream out;
+	BufferedReader in;
+	PrintWriter out;
+	Socket socket;
 	
-	public ClientNetwork()
-	{
-		
+	public void listenSocket(){
+	//Create socket connection
+	   try{
+	     socket = new Socket(hostname, 9001);
+	     out = new PrintWriter(socket.getOutputStream(), true);
+	     in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+	   } catch (UnknownHostException e) {
+	     System.out.println("Unknown host: " + hostname);
+	     System.exit(1);
+	   } catch  (IOException e) {
+	     System.out.println("No I/O");
+	     System.exit(1);
+	   }
 	}
 	
-	private void run() throws IOException {
-		
-        Socket socket = new Socket(hostname, 9001);
-        in = new ObjectInputStream(socket.getInputStream());
-        out = new ObjectOutputStream(socket.getOutputStream());
+	public void run()
+	{
+		String identify; 
+		while(true)
+		{
+			try
+			{
+				identify = in.readLine();
+			}
+			catch (IOException e) 
+			{ 
+				System.out.println("Read failed");
+				System.exit(-1);
+			}
+		}
 	}
 	
-	/*public User getUserShowList(String username,String password) throws IOException
+	public User getUser()
 	{
-		out.writeObject(username);
-		Server 
+		String identify; 
+		while(true)
+		{
+			try
+			{
+				identify = in.readLine();
+			}
+			catch (IOException e) 
+			{ 
+				System.out.println("Read failed");
+				System.exit(-1);
+			}
+		}
+		out
 		User user = new User(username, password);
 		user.getShows();
 		
 		
 		
 		return null;
-	}*/
+	}
+	
+	public TVShow getTVShow()
+	{
+		
+	}
+	
 
 }
--- src/tv/skippable/network/Server.java
+++ src/tv/skippable/network/Server.java
@@ -1,22 +1,52 @@
 package tv.skippable.network;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.Scanner;
+
+import tv.skippable.database.Database;
 
 
 public class Server 
 {
 
-	ObjectInputStream in;
-	ObjectOutputStream out;
+	Scanner in;
+	PrintWriter out;
+	Socket socket;
 	public Server(String hostname) throws UnknownHostException, IOException
 	{
-		Socket socket = new Socket(hostname, 9001);
-        in = new ObjectInputStream(socket.getInputStream());
-        out = new ObjectOutputStream(socket.getOutputStream());
+		 try{
+			 socket = new Socket(hostname, 9001);
+			  } catch (IOException e) {
+			    System.out.println("Could not listen on port 9001");
+			    System.exit(-1);
+			  }
+		
+		 out = new PrintWriter(socket.getOutputStream(), true);
+	     in = new Scanner(new InputStreamReader(socket.getInputStream()));
 	}
 	
+	public void run() throws ClassNotFoundException
+	{
+		String identify;
+		while(true)
+		{
+			try
+			{
+				identify = (String) in.readObject();
+			}
+			catch (IOException e) 
+			{
+				System.out.println("Read failed");
+				System.exit(-1);
+			}
+		}
+	
+	}
 	
 }

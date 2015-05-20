c0f2ba0c598e8cb9cad0e4ed3ce9f1c5d6ce4692
VikHub2016 <s129119@55F9622.rrisd.local>
2015-05-20 13:49:05 -0500

    changed server class

--- src/tv/skippable/network/Server.java
+++ src/tv/skippable/network/Server.java
@@ -3,10 +3,10 @@ package tv.skippable.network;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.PrintWriter;
+import java.io.Reader;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.Scanner;
@@ -21,15 +21,28 @@ import java.net.Socket;
 public class Server 
 {
 
-	ServerSocket serverSocket;
-	int portNumber = 9001;
+		 public static void main(String[] args) throws Exception 
+		 {
+			 ServerSocket listener = new ServerSocket(9090);
+			 try {
+            while (true) {
+                Socket socket = listener.accept();
+                try {
+                    PrintWriter out =
+                        new PrintWriter(socket.getOutputStream(), true);
+                    BufferedReader input =
+                            new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                }finally {
+                    socket.close();
+                }
+            }
+        }
+        finally {
+            listener.close();
+        }
+    }
 	
-	
-	public Server()
-	{
-		serverSocket = new ServerSocket(portNumber);
-	    Socket clientSocket = serverSocket.accept();
-	}
+
 	
 	private void run()
 	{

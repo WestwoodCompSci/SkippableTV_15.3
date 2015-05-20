2c90e8f0f221331eda3c317781ac4ef2ed13a645
LegitMikeyG <michael.goolsbey@gmail.com>
2015-05-20 14:24:48 -0500

    Testing
    I don't think the addEpisode method for the Season class works

--- src/Testing.java
+++ src/Testing.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import tv.skippable.backend.Review;
+import tv.skippable.backend.Season;
 import tv.skippable.backend.TVShow;
 import tv.skippable.backend.Episode;
 
@@ -19,7 +20,7 @@ public class Testing
 		TVShow test = new TVShow("The Office", episodes);
 		List<Review> reviews = new ArrayList<Review>();
 		reviews.add(new Review(5, "10/10"));
-		test.addEpisode(new Episode("title", 1, 1, 23, new ArrayList<Review>()));
+		test.addEpisode(new Episode("title", 1, 1, 23, reviews));
 		test.addEpisode(new Episode("title2", 2, 1, 22, new ArrayList<Review>()));
 		List<Episode> eps = test.getEpisodes();
 		Episode episode = eps.get(0);
@@ -27,6 +28,18 @@ public class Testing
 		//I think there may be a problem with the epsort method. It doesn't seem to work with only one episode
 		
 		System.out.println(episode.getReviews().get(0).getName());
+		System.out.println(episode.getName());
+		System.out.println(eps.get(1).getName());
+		
+		List<Episode> episodes2 = new ArrayList<Episode>();
+		Season s1 = new Season ("The Office", 1, episodes2);
+		s1.addEpisode(test.getEpisodes().get(0));
+		s1.addEpisode(test.getEpisodes().get(1));
+		
+		System.out.println(s1.getLength());
+		s1.setLength();
+		System.out.println(s1.getLength());
+		
 		
 		
 	}

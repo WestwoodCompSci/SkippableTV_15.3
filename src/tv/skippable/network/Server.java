216bcc638df7fc1d2ab33fda7337944c9c24401c
PattyGuo <joowonyong@gmail.com>
2015-05-22 13:00:06 -0500

    More Updates

--- src/tv/skippable/network/Server.java
+++ src/tv/skippable/network/Server.java
@@ -30,38 +30,72 @@ public class Server
 	
 	public Server()
 	{
-		try {
-			ServerSocket listener = new ServerSocket(9090);
-
-			while (true) {
-				socket = listener.accept();
-				out = new ObjectOutputStream(socket.getOutputStream());
-				input = new ObjectInputStream(socket.getInputStream());
-				User user = new User(username, password);
+		Database d = new Database(); //How do we access the database? Just created a new instance of database here to use. Perhaps needs to reivse.
+		while(true)
+		{
+			try {
+				ServerSocket listener = new ServerSocket(9090);
+	
+				while (true) {
+					socket = listener.accept();
+					out = new ObjectOutputStream(socket.getOutputStream());
+					input = new ObjectInputStream(socket.getInputStream());
+					User user = null;
 				
-				String username = (String) input.readObject();
-				if (username.startsWith("GETLIST"))
-				{
-					out.writeObject(user.getList(username.substring(7,username.length()-1)));
+					String username = (String) input.readObject();
+					if (username.startsWith("GETLIST"))
+					{
+						for (int i = 0; i < d.getuserList().size(); i++)
+						{
+							if (d.getuserList().get(i).getUsername().equals(username.substring(7)))
+							{
+								user = d.getuserList().get(i);
+							}
+						}
+						out.writeObject(user.getShows());
+					}
+					else if(username.startsWith("CONFIRM"))
+					{
+						String [] s = username.split("\n");
+						for (int i = 0; i < d.getuserList().size(); i++)
+						{
+							if (d.getuserList().get(i).getUsername().equals(s[0].substring(7)))
+							{
+								if (d.getuserList().get(i).getPassword().equals(s[1]))
+								{
+									out.writeBoolean(true);
+									return;
+								}
+							}
+						}
+						out.writeBoolean(false);
+					}
+					else if(username.startsWith("REGISTER"))
+					{
+						String [] s = username.split("\n");
+						for (int i = 0; i < d.getuserList().size(); i++)
+						{
+							if (d.getuserList().get(i).getUsername().equals(s[0].substring(8)))
+							{
+								out.writeBoolean(false);
+								return;
+							}
+						}
+						//we have to add the user to the database, but there is no adduser() function on the database?
+						
+						
+					}
+					/// read input
+					// relay input to appropriate methods
+					// get return values from methods
+					// write output
+					
 				}
-				else if(username.startsWith("CONFIRM"))
-				{
-					out.writeObject(this.confirm());
-				}
-				else if(username.startsWith("REGISTER"))
-				{
-					out.writeObject(this.register());
-				}
-				/// read input
-				// relay input to appropriate methods
-				// get return values from methods
-				// write output
-				
+	//			listener.close();
 			}
-//			listener.close();
+			catch(Exception e) {}
 		}
-		catch(Exception e) {}
-    }
+	}
 		 
 	
 	
